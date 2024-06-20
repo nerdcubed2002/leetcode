8ab7bd5e11c006af99245be6b4f013081733e4a6
nerdcubed2002 <166093226+nerdcubed2002@users.noreply.github.com>
2024-06-20 09:20:51 +0530

    Time: 12 ms (57.94%), Space: 7.5 MB (93.19%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool possible(vector<vector<char>>& board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == c)
+                return false;
+            if (board[row][i] == c)
+                return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.')
+                    continue;
+
+                for (char num = '1'; num <= '9'; num++) {
+                    if (possible(board, i, j, num)) {
+                        board[i][j] = num;
+                        if (solve(board))
+                            return true;
+                        board[i][j] = '.';
+                    }
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++)
+                cout << board[i][j] << " ";
+            cout << "\n";
+        }
+    }
+};
